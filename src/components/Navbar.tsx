7bb70022b5303efb38a65a80d3530091e8f026b5
arden <ardenhilario@gmail.com>
2022-09-14 07:09:02 +0800

    Add r/all and r/random, highlight active sureddit

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,4 +1,5 @@
 import Link from 'next/link'
+import { useRouter } from 'next/router'
 import { Nav, Navbar as BSNavbar } from 'react-bootstrap'
 import { Subreddit } from '../types/Subreddit'
 import { Thing } from '../types/Thing'
@@ -8,23 +9,35 @@ type Props = {
 }
 
 export default function Navbar({ thingSubreddits }: Props) {
+  const router = useRouter()
+  const { subreddit } = router.query
+
   return (
     <>
       <BSNavbar bg="light" variant="light" className="py-0 px-2 border-bottom border-secondary">
-        <Link href="/">
-          <a className="navbar-brand">Arden</a>
+        <Link href="/" passHref>
+          <BSNavbar.Brand>Arden</BSNavbar.Brand>
         </Link>
 
         <Nav className="me-auto">
-          <Link href="/r/popular">
-            <a className="nav-link">popular</a>
+          <Link href="/r/popular" passHref>
+            <Nav.Link className={`${subreddit === 'popular' ? 'text-orange fw-bold' : ''}`}>popular</Nav.Link>
           </Link>
-
+          <Link href="/r/all" passHref>
+            <Nav.Link className={`${subreddit === 'all' ? 'text-orange fw-bold' : ''}`}>all</Nav.Link>
+          </Link>
+          <Link href="/r/random" passHref>
+            <Nav.Link>random</Nav.Link>
+          </Link>
           {
             thingSubreddits &&
             thingSubreddits.map((thingSubreddit, i) => (
-              <Link key={i} href={thingSubreddit.data.url}>
-                <a className="nav-link text-lowercase">{thingSubreddit.data.display_name}</a>
+              <Link key={i} href={thingSubreddit.data.url} passHref>
+                <Nav.Link className={
+                  `text-lowercase ${thingSubreddit.data.display_name === subreddit ? 'text-orange fw-bold' : ''}`
+                }>
+                  {thingSubreddit.data.display_name}
+                </Nav.Link>
               </Link>
             ))
           }
