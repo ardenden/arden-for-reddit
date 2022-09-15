fd331d5544caf526002d40035c694a77e9c18707
arden <ardenhilario@gmail.com>
2022-09-15 16:00:20 +0800

    Move Subreddit nav and sidebar to layout

--- src/pages/r/[subreddit]/[where]/index.tsx
+++ src/pages/r/[subreddit]/[where]/index.tsx
@@ -1,9 +1,7 @@
 import { NextPage } from 'next'
 import { useRouter } from 'next/router'
 import SubredditPosts from '../../../../components/SubredditPosts'
-import SubredditNav from '../../../../components/SubredditNav'
-import { Col, Nav, Row } from 'react-bootstrap'
-import SubredditSidebar from '../../../../components/SubredditSidebar'
+import { Nav } from 'react-bootstrap'
 import NextLink from 'next/link'
 
 const SubredditWherePage: NextPage = () => {
@@ -13,57 +11,48 @@ const SubredditWherePage: NextPage = () => {
 
   return (
     <>
-      <SubredditNav />
-
-      <Row>
-        <Col className="pe-0">
-          {
-            where === 'comments'
-              ? 'comments page'
-              : where === 'wiki'
-                ? 'wiki page'
-                :
+      {
+        where === 'comments'
+          ? 'comments page'
+          : where === 'wiki'
+            ? 'wiki page'
+            :
+            <>
+              {
+                (where === 'top' || where === 'controversial') &&
                 <>
-                  {
-                    (where === 'top' || where === 'controversial') &&
-                    <>
-                      <Nav defaultActiveKey="hot" activeKey={t as string}>
-                        {
-                          sorts.map((s, i) => (
-                            <Nav.Item key={i}>
-                              <NextLink
-                                href={`/r/${subreddit}/${where}?t=${s}`}
-                                passHref>
-                                <Nav.Link eventKey={s}
-                                  className={
-                                    `nav-link fw-bold ${t === s || (!t && s === 'day')
-                                      ? 'text-gray text-decoration-underline'
-                                      : 'text-blue'}`
-                                  }>
-                                  {
-                                    s === 'all'
-                                      ? 'all time'
-                                      : s === 'day'
-                                        ? 'past 24 hours'
-                                        : `past ${s}`
-                                  }
-                                </Nav.Link>
-                              </NextLink>
-                            </Nav.Item>
-                          ))
-                        }
-                      </Nav>
-                      <hr className="my-0" />
-                    </>
-                  }
-                  <SubredditPosts />
+                  <Nav defaultActiveKey="hot" activeKey={t as string}>
+                    {
+                      sorts.map((s, i) => (
+                        <Nav.Item key={i}>
+                          <NextLink
+                            href={`/r/${subreddit}/${where}?t=${s}`}
+                            passHref>
+                            <Nav.Link eventKey={s}
+                              className={
+                                `nav-link fw-bold ${t === s || (!t && s === 'day')
+                                  ? 'text-gray text-decoration-underline'
+                                  : 'text-blue'}`
+                              }>
+                              {
+                                s === 'all'
+                                  ? 'all time'
+                                  : s === 'day'
+                                    ? 'past 24 hours'
+                                    : `past ${s}`
+                              }
+                            </Nav.Link>
+                          </NextLink>
+                        </Nav.Item>
+                      ))
+                    }
+                  </Nav>
+                  <hr className="my-0" />
                 </>
-          }
-        </Col>
-        <Col className="col-auto ps-0">
-          <SubredditSidebar />
-        </Col>
-      </Row>
+              }
+              <SubredditPosts />
+            </>
+      }
     </>
   )
 }
